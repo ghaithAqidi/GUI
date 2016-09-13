b43ee9cad44e260dc49ba2bfec4ecd67bd85234e
Jonas Larsen <Jonas Larsen>
2016-09-13 15:58:33 +0200

    changed stuff

--- BabyNames/MainWindow.xaml.cs
+++ BabyNames/MainWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace BabyNames
             {
                 Names.Add(new BabyName(line));
             }
-
+            
             // Initialise decades
             var decs = new List<int> {1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000};
             foreach (var dec in decs)
@@ -85,6 +85,9 @@ namespace BabyNames
                     if (babyName.Rank(decade) <= 10 && babyName.Rank(decade) != 0)
                         listForYear.Add(babyName);
                 }
+
+                // Sort list according to rank
+                TopNamesForYear[decade] = listForYear.OrderBy(x => x.Rank(decade)).ToList();
             }
         }
 
@@ -101,5 +104,27 @@ namespace BabyNames
             }
             ListDecadeTopNames.UpdateLayout();
         }
+
+        private void SerachButton_Click(object sender, RoutedEventArgs e)
+        {
+            var name = NameBox.Text;
+
+            var babyName = Names.FirstOrDefault(x => x.Name.ToLower().Equals(name.ToLower()));
+
+            if (babyName == null)
+                return;
+
+            // Set labels for avg and trend
+            TrendResLabel.Content = babyName.Trend();
+            RankResLabel.Content = babyName.AverageRank();
+
+            YearRankBox.Items.Clear();
+            foreach (var decade in DecadesList)
+            {
+                YearRankBox.Items.Add($"{decade} - {babyName.Rank(decade)}");
+            }
+            YearRankBox.UpdateLayout();
+        }
+        
     }
 }
